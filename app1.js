7e97f1c886728c09eda56b98370d9822951b934f
Connor Curtis <concur1998@gmail.com>
2021-01-07 17:38:09 -0800

    fixed a function on app1.js

--- app1.js
+++ app1.js
@@ -1,16 +1,15 @@
 let userName ='';
-let message ='';
 
 function type(){
 userName = prompt('Please Enter Your Name');
 
-document.write('Welcome' + userName);
+document.write(' Welcome ' + userName);
 }
 
 
 function message1(){
     if (userName == 'Connor'){
-        document.write('<h3>' + 'ayyylmao' + '</h3>');
+        document.write('<h3>' + 'ayyy' + '</h3>');
     } else {
         document.write('<h3>' + 'Welcome Guest' + '</h3>');
     }
@@ -18,8 +17,8 @@ function message1(){
 
 
 function message2(){
-    stringVar = prompt("My site is not finished yet, okay? Type, 'ok' to continue.");
-    if (message = 'ok'){
+    stringVar = prompt("My site is not finished yet, ok? Type, 'ok' to continue.");
+    if (stringVar == 'ok'){
         alert('Welcome');
     } else {
         alert('please exit this site');    
